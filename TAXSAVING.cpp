a739932ad44e77d4e03652607b3fe2a608559756
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-19 12:10:53 +0600

    Add files via upload

---
+++ TAXSAVING.cpp
@@ -0,0 +1,17 @@
+/*----->>> Mahady Hasan Fahim
+*/
+//https://www.codechef.com/problems/TAXSAVING
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ int t;
+ cin>>t;
+ while(t--){
+    int x,y;
+    cin>>x>>y;
+    cout<<x-y<<endl;
+ }  
+   return 0;
+}
