b7f96120b5437e8325bce175c68e5e23e6710a6a
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-19 12:11:06 +0600

    Add files via upload

---
+++ KNGTOR.cpp
@@ -0,0 +1,18 @@
+/*----->>> Mahady Hasan Fahim
+*/
+//https://www.codechef.com/problems/KNGTOR
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int t;
+     cin>>t;
+     while(t--){
+        int n,m;
+        cin>>n>>m;
+        cout<<n*5+m*7<<endl;
+     }
+   
+   return 0;
+}
