ee556dca76086db505e1b46e5141f4f962863171
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-19 12:11:55 +0600

    Add files via upload

---
+++ JERRYCHASE.cpp
@@ -0,0 +1,22 @@
+/*----->>> Mahady Hasan Fahim
+*/
+//https://www.codechef.com/problems/JERRYCHASE
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x>=y){
+            cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+        }
+    }
+   
+   return 0;
+}
