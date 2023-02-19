74ee597e4faa92f47585f303a2206bca308f86c9
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-19 19:30:24 +0600

    Add files via upload

---
+++ WAITTIME.cpp
@@ -0,0 +1,18 @@
+/*----->>> Mahady Hasan Fahim
+*/
+//https://www.codechef.com/problems/WAITTIME
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        cout<<(x*7)-y<<endl;
+    }
+   
+   return 0;
+}
