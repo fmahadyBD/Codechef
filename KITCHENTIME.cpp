7c1374e7509e05f0f2f2368cd41dc923cb6736b0
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-19 12:10:17 +0600

    Add files via upload

---
+++ KITCHENTIME.cpp
@@ -0,0 +1,17 @@
+/*----->>> Mahady Hasan Fahim
+*/
+//https://www.codechef.com/problems/KITCHENTIME
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ int t;
+ cin>>t;
+ while(t--){
+    int x,y;
+    cin>>x>>y;
+    cout<<y-x<<endl;
+ }  
+   return 0;
+}
