dde362b24045075e3d7fb1c4e02b83957e574e5a
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-19 19:29:29 +0600

    Add files via upload

---
+++ expriment.cpp
@@ -0,0 +1,20 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    
+    for(int i=0;i<5;i++){
+    if(i>=1){
+        if(i==2){
+            break;
+        }
+    }
+        cout<<"HI"<<endl;
+    }
+   
+   return 0;
+}
