183d214e6c5a64c13fbd8f5473ec935996b4132c
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-19 12:10:41 +0600

    Add files via upload

---
+++ REACHTARGET.cpp
@@ -0,0 +1,20 @@
+/*----->>> Mahady Hasan Fahim
+*/
+//https://www.codechef.com/problems/REACHTARGET
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ int t;
+ cin>>t;
+ while (t--)
+ {
+   int x,y;
+   cin>>x>>y;
+   cout<<x-y<<endl;
+
+ }
+   
+   return 0;
+}
