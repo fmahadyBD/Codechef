f97a8b261eba6604ea717bddce5a6a1c425c9bed
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-21 23:40:41 +0600

    Add files via upload

---
+++ SUGARCANE.cpp
@@ -0,0 +1,24 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--){
+   float n;
+   cin>>n;
+   float nm=n*50;
+   float co2=(float)(30/100);
+   cout<<co2;
+   float co=co2*nm;
+   cout<<co<<endl;
+   
+   
+   
+ }
+   return 0;
+}
