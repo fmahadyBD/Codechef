06089f2602d9b6501e6ddfd6104c5138e5bf0a09
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-21 23:37:35 +0600

    Add files via upload

---
+++ NOTDIVISIBLE.cpp
@@ -0,0 +1,21 @@
+/*----->>> Mahady Hasan Fahim
+*/
+//https://www.codechef.com/problems/NOTDIVISIBLE?tab=statement
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ int t;
+ while(t--)  {
+    int n;
+    int ar[n];
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+
+        }
+
+    }
+ }
+   return 0;
+}
