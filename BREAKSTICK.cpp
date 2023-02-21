b5ce898d0960bff64e71fc5e533625e509d752f1
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-21 23:39:18 +0600

    Add files via upload

---
+++ BREAKSTICK.cpp
@@ -0,0 +1,19 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--){
+      int n,x;
+      cin>>n>>x;
+      if(n%x==0){
+         cout<<"YES";
+      }else if()
+   }
+   return 0;
+}
