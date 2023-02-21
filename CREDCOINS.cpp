971577dadf94e055e3ba39e4263782ba0149a119
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-21 23:39:30 +0600

    Add files via upload

---
+++ CREDCOINS.cpp
@@ -0,0 +1,19 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--){
+   int x,y;
+   cin>>x>>y;
+   cout<<(x*y)/100<<endl;
+   
+   
+ }
+   return 0;
+}
