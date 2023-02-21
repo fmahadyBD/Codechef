6077a5a29b320c5887845d8d033fd136167820b6
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-21 23:37:54 +0600

    Add files via upload

---
+++ HEIGHTRATION.cpp
@@ -0,0 +1,18 @@
+/*----->>> Mahady Hasan Fahim
+*/
+//https://www.codechef.com/problems/HEIGHTRATION
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+   
+   int a,b;
+   cin>>a>>b;
+   if(a>b){
+    cout<<a<<endl;
+   }else if(b>a){
+    cout<<b<<endl;
+   }
+   return 0;
+}
