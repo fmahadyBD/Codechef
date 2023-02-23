71d51f4e6293d6a89bf8d3d99a7bbcd61924ce56
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-02-23 09:12:55 +0600

    Add files via upload

---
+++ NOTEBOOK.cpp
@@ -0,0 +1,19 @@
+/*----->>> Mahady Hasan Fahim
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--){
+   
+   int a;
+   cin>>a;
+   cout<<(a*1000)/100<<endl;
+   
+ }
+   return 0;
+}
